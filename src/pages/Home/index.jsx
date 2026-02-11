300cca6451ffd21f4fb7f0832c4d6b035604fb74
Renan Ponick <renan.ponick@ngi.com.br>
2026-02-11 17:39:49 -0300

    feat: add useEffect

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+
 export default function Home() {
+  const [message, setMessage] = useState("Carregando mensagem...");
+
+  useEffect(() => {
+    // Simula uma chamada de API
+    const timer = setTimeout(() => {
+      setMessage("Bem-vindo à nossa aplicação React 🚀");
+    }, 2000);
+
+    // Função de limpeza (cleanup)
+    return () => {
+      clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <div className="page home">
+    <div className="home">
       <section className="hero">
         <h2>Bem-vindo 👋</h2>
-        <p>
-          Esta é a página inicial da aplicação. Aqui você apresenta a proposta
-          principal, orienta o usuário e cria contexto.
-        </p>
+        <p>{message}</p>
       </section>
 
       <section className="features">
